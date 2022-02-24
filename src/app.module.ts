6df6d4c5527283cf742d92844160a359590f23cf
Sergey Zenin <sergey_zenin@epam.com>
2022-02-24 12:07:05 +0300

    init app depending on env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Logger, Module, Provider } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks';
@@ -10,11 +10,32 @@ import { APP_GUARD } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigurationModule } from '../config/configuration.module';
 import { DbService } from './db/db.service';
+import { Environment } from './types';
+
+const environment = process.env.NODE_ENV as Environment;
+
+const envFilePathMap: Record<Environment, ConfigModuleOptions['envFilePath']> =
+  {
+    production: '.env',
+    debug: '',
+    development: ['.env.development.local', '.env.development', '.env'],
+  };
+
+let providers: Provider[] = [AppService, Logger];
+
+const productionProviders: Provider[] = [
+  { provide: APP_GUARD, useClass: JwtAuthGuard },
+  { provide: APP_GUARD, useClass: RolesGuard },
+];
+
+if (environment === 'production') {
+  providers = [...providers, ...productionProviders];
+}
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      envFilePath: envFilePathMap[environment],
       load: [configuration],
     }),
     TasksModule,
@@ -26,11 +47,6 @@ import { DbService } from './db/db.service';
     }),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    Logger,
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-  ],
+  providers,
 })
 export class AppModule {}
